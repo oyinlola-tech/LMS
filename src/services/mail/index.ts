2a7ec2174a5a8af90b07e83eb5a39f6f04bbbd32
Oyinlola-tech <oyinlola.tech@icloud.com>
2026-07-24 11:37:35 +0100

    Update mail service index

--- src/services/mail/index.ts
+++ src/services/mail/index.ts
@@ -3,3 +3,5 @@ export { baseTemplate, escapeHtml } from './baseTemplate';
 export { transporter, SMTP_FROM_ADDRESS } from './transporter';
 export { sendEmail, sendEmailNow, renderTemplate } from './sendMail';
 export { templates } from './templates';
+export * from './commands';
+export * from './queries';
